bc8564b91ab86c5f371ecf3945f56905a3a346b8
Dasun Navindu <dasunnavindu2001@gmail.com>
2024-08-06 11:32:18 +0530

    start

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+  setTimeout(function() {
+      var image = document.getElementById('delayedImage');
+      image.classList.add('show');
+  }, 500); 
+});
