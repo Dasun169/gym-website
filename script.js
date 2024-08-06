c6b80d03938c686d1ced7cc35d4f0020c5d4bbac
Dasun Navindu <dasunnavindu2001@gmail.com>
2024-08-07 01:05:11 +0530

    html/css

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ window.addEventListener('load', function() {
       image.classList.add('show');
   }, 500); 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.querySelectorAll('.hidden').forEach(function(element) {
+      element.classList.remove('hidden');
+      element.classList.add('fade-in');
+    });
+  }, 1000);
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    document.querySelectorAll('.hidden').forEach(function(element) {
+      element.classList.remove('hidden');
+      element.classList.add('fade-in');
+    });
+  }, 1000);
+});
